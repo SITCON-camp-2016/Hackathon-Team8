33ac29d12948f1063d304307d407fbf9ceb5ee66
purinhow7 <dglpw7546@gmail.com>
2016-08-05 01:11:50 +0800

    Nightmare

--- activity_set_JQ.js
+++ activity_set_JQ.js
@@ -1,9 +1,14 @@
-$('#tosubmit').on('submit',function(){
+/*$('#tosubmit').on('submit',function(){
 	$.ajax({
 		url:
 		data: $('#tosubmit').serialize(),
 		type:"POST",
 		dataType:"GET"
+	})
+	return false;
+})*/
+$('#tosubmit').on('submit',function(){
+	location.href="http://www.centerbbs.com";
 	})
 	return false;
 })
